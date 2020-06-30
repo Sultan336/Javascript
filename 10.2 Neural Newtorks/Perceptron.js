887986006d8e0677a4b07a1a54b19ffd35571bad
Sultan <m.sultan338@gmail.com>
2020-06-30 22:05:54 +0500

    add comments, more code

--- 10.2 Neural Newtorks/Perceptron.js
+++ 10.2 Neural Newtorks/Perceptron.js
@@ -1,7 +1,8 @@
 class Perceptron {
     float[] weights = new float[2];
-
-    void Perceptron() {
+    // Constructor
+    Perceptron() {
+        // Initialize the weights randomly        
         for (int i = 0; i < weights.length; i++) {
             weights. [i] = random(-1, 1);
         }
