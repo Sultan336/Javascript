8bccb59102591a3125ab4a80054527b04a4f4956
Sultan <m.sultan338@gmail.com>
2020-06-30 22:20:36 +0500

    add code

---
+++ 10.2 Neural Newtorks/Neural_Newtwork.js
@@ -0,0 +1,16 @@
+Perceptron P;
+
+void setup() {
+    size(200, 200);
+    P = new Perceptron();
+    float inputs = {
+        -1,
+        0.5
+    }
+    int guess = p.guess(inputs);
+    console.log(guess)
+}
+
+void draw() {
+
+}
